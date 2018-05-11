e51ea37f98557b8ac52989ad55b0e905350c003b
AlexPeralta <alexandra.peralta1@gmail.com>
2018-05-11 16:49:03 -0400

    dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
