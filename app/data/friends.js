f304c38b59b1ee7c374f8353e5ff092e67c687b1
AlexPeralta <alexandra.peralta1@gmail.com>
2018-05-12 01:01:03 -0400

    adds best match functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 var friends = [
     {
         name:"Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo:"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
         scores:[
             5,
             1,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,10 +9,32 @@ module.exports = function(app){
     
     //Create and save new friends with POST
     app.post("/api/friends", function(req, res){
-        var newFriend = req.body;
-        console.log(newFriend);
+        var newFriend = req.body;        
+
+        //here we find a match for our new friend profile, initializing with the first friend we have
+        var bestMatch = friends[0];
+        var lowestRunningDifference = 41;
+
+        //loop through profiles
+        friends.forEach(friend => {
+            var tallyDifference = 0;
+            //loop through each question score
+            for(var i = 0; i < friend.scores.length; i++){
+                tallyDifference += Math.abs(friend.scores[i] - newFriend.scores[i]);
+
+                if(tallyDifference <= lowestRunningDifference){
+                    //we have a new lowest difference, this could be a match made in heaven
+                    lowestRunningDifference = tallyDifference;
+                    bestMatch = friend;
+                }
+            }
+
+            
+        });
+        //api sends back the match
+        res.send(bestMatch);
+        //lastly add new user to friends array
         friends.push(newFriend);
-        res.json(newFriend);
     });
      
     //clear out table
